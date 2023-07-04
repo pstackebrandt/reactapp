c2076e2ef14c54bb28bc5348a659c066fdb19a4e
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-04 15:26:59 +0200

    Add Use click event

--- src/App_use_state.js
+++ src/App_use_state.js
@@ -8,17 +8,10 @@ const App = () => {
   console.log(myState); // log the value
   console.log(setMyState); // log the update function for myState
   
-  const decrement = () => {
-    setMyState(--myState);
-  }
-
   return (
     <>
       <h1>Welcome awakened one!</h1>
       <p>Minutes you needed to fall asleep {myState} (number saved in a state)</p>
-      <button onClick={decrement}>
-        Fall asleep a little faster!
-      </button>
     </>
   )
 }
