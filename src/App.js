6670587f35e6eccb39acd88de937f68d74ddf4bd
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-04 14:33:50 +0200

    Send simplel greeting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Diese Funktion muss Groß geschrieben werden
+// Der Rückgabewert der Funktion beschreibt die Komponente
+const App = () => {
+  // Es wird JSX-Code zurückgegeben.
+  // Das ist eine an HTML angelehnte Templatesprache
+  // Das Fragment <></> ist eine logische Zusammenfassung, die nicht auf der Webseite benutzt wird
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Peters Website</h1>
+      <p>Good morning friend!</p>
+    </>
+  )
 }
 
 export default App;
